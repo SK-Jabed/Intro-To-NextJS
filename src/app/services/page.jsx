009605c330ccf875abbc11fa42f654b90fb656d9
Sheikh Jabed <jabedbd2295@gmail.com>
2025-02-04 23:48:07 +0600

    Services Page and Routing for This Page is Completed

---
+++ src/app/services/page.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const page = () => {
+    return (
+        <div>
+            <h2 className='font-semibold text-3xl'>Services page Here</h2>
+        </div>
+    );
+};
+
+export default page;
