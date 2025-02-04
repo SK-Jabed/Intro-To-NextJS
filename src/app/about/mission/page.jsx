4117ad257d994b508a7cbc6426fa4ac22fe91bce
Sheikh Jabed <jabedbd2295@gmail.com>
2025-02-04 23:57:02 +0600

    Mission and Vision Named Nested Routes Added

---
+++ src/app/about/mission/page.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const MissionPage = () => {
+    return (
+        <div>
+            Our mission is here
+        </div>
+    );
+};
+
+export default MissionPage;
---
+++ src/app/about/vision/page.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const VisionPage = () => {
+    return (
+        <div>
+            Vision is here
+        </div>
+    );
+};
+
+export default VisionPage;
