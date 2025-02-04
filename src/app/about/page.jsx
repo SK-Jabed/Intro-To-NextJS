8c7942fa5da99ea4b0f6687ff2e90c885eb5d1be
Sheikh Jabed <jabedbd2295@gmail.com>
2025-02-04 23:58:38 +0600

    Mission and Vision Named Nested Routes Added

--- src/app/about/page.jsx
+++ src/app/about/page.jsx
@@ -4,6 +4,22 @@ const page = () => {
     return (
         <div>
             About Page is here
+            <li>
+        <Link
+          href={"/about/mission"}
+          className="text-lg font-medium text-gray-800 hover:text-bg-cyan-400"
+        >
+          Mission
+        </Link>
+      </li>
+      <li>
+        <Link
+          href={"/about/services"}
+          className="text-lg font-medium text-gray-800 hover:text-bg-cyan-400"
+        >
+          Services
+        </Link>
+      </li>
         </div>
     );
 };
