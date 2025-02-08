87685e2c93ffe83f8c1bcec1644939ebc9c98849
Sheikh Jabed <jabedbd2295@gmail.com>
2025-02-08 14:06:13 +0600

    Footer Component Added and Design Completed

--- src/app/about/page.jsx
+++ src/app/about/page.jsx
@@ -7,24 +7,23 @@ const page = () => {
       About Page is here
       <div>
         <ul className="flex justify-between">
-<li>
-          <Link
-            href={"/about/mission"}
-            className="text-lg font-medium text-gray-800 hover:text-bg-cyan-400"
-          >
-            Mission
-          </Link>
-        </li>
-        <li>
-          <Link
-            href={"/about/vision"}
-            className="text-lg font-medium text-gray-800 hover:text-bg-cyan-400"
-          >
-            Vision
-          </Link>
-        </li>
+          <li>
+            <Link
+              href={"/about/mission"}
+              className="text-lg font-medium text-gray-800 hover:text-bg-cyan-400"
+            >
+              Mission
+            </Link>
+          </li>
+          <li>
+            <Link
+              href={"/about/vision"}
+              className="text-lg font-medium text-gray-800 hover:text-bg-cyan-400"
+            >
+              Vision
+            </Link>
+          </li>
         </ul>
-        
       </div>
     </div>
   );
