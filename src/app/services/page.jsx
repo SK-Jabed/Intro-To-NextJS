88e4a4145071a3fd462c3a332b4f1c1028c2d0dd
Sheikh Jabed <jabedbd2295@gmail.com>
2025-02-08 23:09:46 +0600

    Service Page's Card Design Completed

--- src/app/services/page.jsx
+++ src/app/services/page.jsx
@@ -2,7 +2,7 @@ import Link from "next/link";
 import React from "react";
 
 const page = () => {
-   const services = [
+  const services = [
     {
       _id: "1",
       service_name: "Web Development",
@@ -72,47 +72,36 @@ const page = () => {
   ];
 
   return (
-    <div>
-      <h2 className="font-semibold text-3xl">Services page Here</h2>
-      {/* {
-        services.map(service => {
-            return (
-               <div>
-                <p>{service.service_name}</p>
-                <Link href={`/services/${service._id}`}>
-                
-                <img src={service.service_image} alt="" />
-                </Link>
-            </div> 
-            )           
-        })
-      } */}
+    <div className="my-14">
+      <h2 className="font-semibold text-3xl text-center mb-2">All Available Services</h2>
+      <p className="text-center text-gray-500 mb-6">Explore our all available services and take a bow!</p>
+      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6 w-11/12 mx-auto">
+  {services.map((service) => (
+        <div className="p-6 bg-white rounded-lg shadow-lg hover:shadow-2xl transition-transform transform hover:scale-105 h-[520px]">
+          <img
+            src={service.service_image}
+            alt={service.service_name}
+            className="w-full h-56 object-cover rounded-t-lg"
+          />
+          <h3 className="text-xl font-bold mt-4">{service.service_name}</h3>
+          <p className="text-gray-700 mt-2">{service.service_description}</p>
+          <div className="mt-4">
+            <p className="text-sm mt-2">Category: {service.category}</p>
+            <p className="text-sm">Availability: ${service.availability}</p>
+            <p className="text-sm">Price Range: ${service.price_range}</p>
+            <p className="text-sm">Rating: ${service.rating}</p>
 
-      {services.map((service) => (
-            <div className="p-6 bg-white rounded-lg shadow-lg hover:shadow-2xl transition-transform transform hover:scale-105 h-[520px]">
-              <img
-                src={service.service_image}
-                alt={service.service_name}
-                className="w-full h-56 object-cover rounded-t-lg"
-              />
-              <h3 className="text-xl font-bold mt-4">{service.service_name}</h3>
-              <p className="text-gray-700 mt-2">{service.service_description}</p>
-              <div className="mt-4">
-                <p className="text-sm mt-2">
-                  Category: {service.category}
-                </p>
-                <p className="text-sm">
-                  : ${service.minDonation}
-                </p>
-                <button
-                  onClick={() => navigate(`/service/${service._id}`)}
-                  className="px-8 py-3 rounded-full bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500 text-white font-bold shadow-lg hover:shadow-xl hover:bg-gradient-to-l hover:from-pink-500 hover:via-purple-500 hover:to-indigo-500 transition duration-300 ease-in-out mt-2"
-                >
-                  See More
-                </button>
-              </div>
-            </div>
-        ))}
+            <Link href={`/services/${service._id}`}>
+              <button className="px-8 py-3 rounded-full bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500 text-white font-bold shadow-lg hover:shadow-xl hover:bg-gradient-to-l hover:from-pink-500 hover:via-purple-500 hover:to-indigo-500 transition duration-300 ease-in-out mt-2">
+                See More
+              </button>
+            </Link>
+          </div>
+        </div>
+      ))}
+      </div>
+
+    
     </div>
   );
 };
