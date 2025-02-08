56ecf7504985e5f741243762e58bf9bb86f55cde
Sheikh Jabed <jabedbd2295@gmail.com>
2025-02-08 23:52:06 +0600

    Services Added and Dynamic Routing with Details Page Done

--- src/app/about/vision/page.jsx
+++ src/app/about/vision/page.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 const VisionPage = () => {
     return (
         <div>
-            <h2 className='text-3xl font-semibold'>Vision is here</h2>
+            <h2 className='text-3xl font-semibold'>Our Vision is here</h2>
         </div>
     );
 };
