6dda708c18eeb708f50b6e3ba5ea3a1cfb0cb47d
Sheikh Jabed <jabedbd2295@gmail.com>
2025-02-24 22:43:11 +0600

    Profile pages design updated

--- src/app/profile/page.jsx
+++ src/app/profile/page.jsx
@@ -1,12 +1,12 @@
-import React from 'react';
+import React from "react";
 
 const ProfilePage = () => {
-    return (
-        <div>
-            <h2 className='font-semibold text-2xl'>User's Profile is here</h2> 
-            <p className='text-xl font-bold'>Profile Page</p>
-        </div>
-    );
+  return (
+    <div>
+      <h2 className="font-semibold text-2xl">User's Profile is here</h2>
+      <p className="text-xl font-bold">Profile Page</p>
+    </div>
+  );
 };
 
 export default ProfilePage;
